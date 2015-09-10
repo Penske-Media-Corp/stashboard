14bfccbf67566111c57a7a6c4cfa85ac2703a757
Gabriel Koen <gabriel.koen@gmail.com>
2015-09-09 17:13:10 -0700

    More updates for PMC

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -2,17 +2,17 @@ import os
 
 DEBUG = False
 
-SITE_NAME = "Stashboard"
-SITE_AUTHOR = "Colonel Mustache"
-SITE_URL = "http://stashbooard.appspot.com"
-REPORT_URL = "mailto:help@stashboard.org"
+SITE_NAME = "PMC Status Board"
+SITE_AUTHOR = "PMC Engineering"
+SITE_URL = "http://status.pmc.com"
+REPORT_URL = "http://helpdesk.pmc.com/"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_ACCESS_TOKEN = ''
 TWITTER_ACCESS_TOKEN_SECRET = ''
-TWITTER_HANDLE = 'stashboard_status'
+TWITTER_HANDLE = 'pmceng_status'
 
 # RSS Feed settings
 RSS_NUM_EVENTS_TO_FETCH = 50
